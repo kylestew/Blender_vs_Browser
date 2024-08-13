034839566042bf8cf4fee3441af95a664bb266cc
Kyle Stewart <kylestew@gmail.com>
2024-08-13 14:06:17 +0100

    Generalized subdivide cube function

--- web-to-blender/src/sketches/cubism_raw_rendered.ts
+++ web-to-blender/src/sketches/cubism_raw_rendered.ts
@@ -1,39 +1,57 @@
 import { Cube, CubeGrid } from 'root/geo'
 import { offset } from 'root/geo'
-import { random } from 'root/random'
+import { random, pickRandom } from 'root/random'
 import { BlenderRemote } from '../blender_remote'
 
 const remote = new BlenderRemote()
-let finalCount = 0
-
-const sideCount = 6
+let outputCount = 0
 
 // spacing between cubes
 const padding = 0.025
 
-function subdivide(cube: Cube) {
-    const grid = CubeGrid.withCube(cube, 2, 2, 2)
+function divisionCount(generation: number) {
+    if (generation === 0) {
+        // return 6
+        return 3
+    } else if (generation === 1) {
+        return pickRandom([2, 3])
+    }
+    return 4
+}
+
+function subdivideDecision(generation: number): boolean {
+    if (generation === 0) {
+        return random() < 0.5
+    } else if (generation === 1) {
+        return random() < 0.05
+    }
+    return false
+}
+
+function randomRemoveDecision(generation: number): boolean {
+    return random() < 0.3333
+}
+
+function subdivideCube(cube: Cube, generation: number = 0) {
+    const divisions = divisionCount(generation)
+    const grid = CubeGrid.withCube(cube, divisions, divisions, divisions)
+
     grid.cubes().forEach((cube) => {
-        const insetCube = offset(cube, [-padding / 2.0, -padding / 2.0, -padding / 2.0])
-        remote.addObject(insetCube)
-        finalCount++
+        if (subdivideDecision(generation)) {
+            subdivideCube(cube, generation + 1)
+        } else {
+            if (!randomRemoveDecision(generation)) {
+                const insetCube = offset(cube, [-padding / 2.0, -padding / 2.0, -padding / 2.0])
+                remote.addObject(insetCube)
+                outputCount++
+            }
+        }
     })
     remote.flush()
 }
 
-const grid = new CubeGrid([0, 0, 0], [2.0, 2.0, 2.0], sideCount, sideCount, sideCount)
-grid.cubes().forEach((cube) => {
-    const insetCube = offset(cube, [-padding, -padding, -padding]) as Cube
-
-    // throw to keep or subdivide
-    if (random() < 0.4) {
-        // subdivide into 2x2
-        subdivide(insetCube)
-    } else {
-        remote.addObject(insetCube)
-        finalCount++
-    }
-})
+const baseCube = new Cube([0, 0, 0], [2, 2, 2])
+subdivideCube(baseCube)
 
 remote.flush()
-console.log('Final count:', finalCount)
+console.log('Final count:', outputCount)
